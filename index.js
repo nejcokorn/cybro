a48a6a3ead241182ac65e78ea940aa97d9a2fa38
nejc.okorn <nejc.okorn@gmail.com>
2021-11-28 14:47:03 +0100

    Idea of listener was moved to CybroComm, so there is no need for Cybro listener class

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
 const { CybroController } = require('./controller');
 const { CybroComm } = require('./comm');
-const dgram = require('dgram');
-const EventEmitter = require('events');
 
-// Single point where socket is maintained
-class Cybro extends EventEmitter {
-	constructor(port = 8442) {
-		super();
+controllers = [];
 
-		this.port = port;
-		this.controllers = [];
-
-	}
-
-	async addController(address, nad, password) {
+module.exports = {
+	getController: (address, port, nad, password) => {
 		// Check if controller object has already been created
-		let controller = this.controllers.find((controller) => {
+		let controller = controllers.find((controller) => {
 			return controller.address == address;
 		});
 		
@@ -25,32 +16,24 @@ class Cybro extends EventEmitter {
 		}
 		
 		// Create new controller object when 
-		controller = new CybroController(address, this.port, nad, password);
+		controller = new CybroController(address, port, nad, password);
 
 		// Connect with controller
 		await controller.connect();
 
 		// Add to the list of controllers
-		this.controllers.push(controller);
+		controllers.push(controller);
 
 		// Run configuration on the controller
-		console.log(`Controler ${controller.address} - ${controller.nad}: Reading controller configuration`);
 		await controller.readConfiguration();
-		console.log(`Controler ${controller.address} - ${controller.nad}: DONE Reading controller configuration`);
 
 		// Return new controller
 		return controller;
-	}
-
-	static async autodetect(port = 8442) {
+	},
+	
+	autodetect: async (port = 8442) => {
 		// Autodetect Cybro controllers
-		let controllers = await CybroComm.autodetect(port);
-
-		// Return list of controllers
-		return controllers;
+		// Return list of Cybro controllers
+		return await CybroComm.autodetect(port);
 	}
-}
-
-module.exports = {
-	Cybro
 }
